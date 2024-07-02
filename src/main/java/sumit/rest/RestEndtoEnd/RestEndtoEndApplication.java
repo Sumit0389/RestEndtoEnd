b43d2af1ab4b18f8a5d625991870c5a958bb7c68
Sumit0389 <sumit.s0389@gmail.com>
2024-07-02 17:50:44 +0530

    new Springboot Project

---
+++ src/main/java/sumit/rest/RestEndtoEnd/RestEndtoEndApplication.java
@@ -0,0 +1,13 @@
+package sumit.rest.RestEndtoEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestEndtoEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestEndtoEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sumit/rest/RestEndtoEnd/RestEndtoEndApplicationTests.java
@@ -0,0 +1,13 @@
+package sumit.rest.RestEndtoEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestEndtoEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
